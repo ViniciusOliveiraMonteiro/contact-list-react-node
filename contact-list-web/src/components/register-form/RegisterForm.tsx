9a02c780aa7c6be5cac76a5ef6452f914e3af208
Vinicius Monteiro <viniciussppt@gmail.com>
2023-05-16 17:54:12 -0300

    Adicionado validação no formulário de cadastro

--- contact-list-web/src/components/register-form/RegisterForm.tsx
+++ contact-list-web/src/components/register-form/RegisterForm.tsx
@@ -13,7 +13,7 @@ interface RegisterFormProps {
   setUpdatedState?:React.Dispatch<React.SetStateAction<ContactCards>>;
 }
 
-interface ISubmitForm {
+interface ISubmitForm extends FormEvent {
   fullname: string;
   numberPhone: string;
   email: string;
@@ -29,9 +29,20 @@ export function RegisterForm(props: RegisterFormProps){
   const [companyName, setCompanyName] = useState('');
   const service = new RegisterFormService();
   const dashboardService = new ContactDashboardService();
-  const validationScheme = Yup.object().shape({
+  const validationSchema = Yup.object().shape({
     fullName: Yup.string().required('Nome Completo é obrigatório'),
-    numberPhone: Yup.string().trim().required('Telefone é obrigatório'),
+    numberPhone: Yup.string().required('Telefone é obrigatório'),
+    email: Yup.string().required('Email é obrigatório').email('Este email é inválido'),
+    occupation: Yup.string().required('Cargo é obrigatório'),
+    companyName: Yup.string().required('Empresa é obrigatório'),
+  });
+  const { 
+    register, 
+    handleSubmit, 
+    reset, 
+    formState: { errors } 
+  } = useForm<ISubmitForm>({
+    resolver: yupResolver(validationSchema)
   });
 
   async function submitFormAction(event: FormEvent){
@@ -47,7 +58,6 @@ export function RegisterForm(props: RegisterFormProps){
     const dashboardResponse = await dashboardService.listContact();
     if((props.dialogState && response.success)){
       if(props.setUpdatedState && dashboardResponse.data){
-        console.log(dashboardResponse, dashboardResponse.data);
         props.setUpdatedState(dashboardResponse.data);
       }
       props.dialogState(false);
@@ -55,23 +65,26 @@ export function RegisterForm(props: RegisterFormProps){
   }
 
   return(
-    <form onSubmit={submitFormAction} className='d-flex flex-column p-3'>
+    <form onSubmit={handleSubmit(submitFormAction)} className='d-flex flex-column p-3'>
       <div className='d-flex flex-column mb-2'>
         <label htmlFor="">Nome completo</label>
         <input 
           type="text" 
-          className={`${style.customInput}`} 
+          className={`form-control ${style.customInput} ${errors.fullname ? 'is-invalid' : ''}`}
+          {...register('fullname')}
           value={fullName}
           onChange={(event)=>setFullName(event.target.value)}
         />
+        <div className="invalid-feedback">{errors.fullname?.message}</div>
       </div>
       <div className='d-flex flex-column mb-2'>
         <label htmlFor="">Telefone</label>
         <InputMask 
           mask={numberPhone.replace(/[^\d]/g, '').length >= 6 && numberPhone[3] == '9' ? '99 99999-9999' : '99 9999-9999'} 
           maskPlaceholder={null}
-          className={`${style.customInput}`}
+          className={`form-control ${style.customInput} ${errors.numberPhone ? 'is-invalid' : ''}`}
           value={numberPhone}
+          {...register('numberPhone')}
           onChange={(event)=>{
             setNumberPhone(event.target.value);
           }}
@@ -86,33 +99,40 @@ export function RegisterForm(props: RegisterFormProps){
             }
           }}
         />
+        <div className="invalid-feedback">{errors.numberPhone?.message}</div>
       </div>
       <div className='d-flex flex-column mb-2'>
         <label htmlFor="">Email</label>
         <input 
           type="text" 
-          className={`${style.customInput}`} 
+          className={`form-control ${style.customInput} ${errors.email ? 'is-invalid' : ''}`} 
           value={email}
+          {...register('email')}
           onChange={(event)=>setEmail(event.target.value)}
         />
+        <div className="invalid-feedback">{errors.email?.message}</div>
       </div>
       <div className='d-flex flex-column mb-2'>
         <label htmlFor="">Cargo</label>
         <input 
           type="text" 
-          className={`${style.customInput}`} 
+          className={`form-control ${style.customInput} ${errors.occupation ? 'is-invalid' : ''}`} 
           value={occupation}
+          {...register('occupation')}
           onChange={(event)=>setOccupation(event.target.value)}
         />
+        <div className="invalid-feedback">{errors.occupation?.message}</div>
       </div>
       <div className='d-flex flex-column '>
         <label htmlFor="">Empresa</label>
         <input 
           type="text" 
-          className={`${style.customInput}`} 
+          className={`form-control ${style.customInput} ${errors.occupation ? 'is-invalid' : ''}`} 
           value={companyName}
+          {...register('companyName')}
           onChange={(event)=>setCompanyName(event.target.value)}
         />
+        <div className="invalid-feedback">{errors.companyName?.message}</div>
       </div>
       <div className='d-flex justify-content-end mt-4'>
         <button type='submit' className={`${style.submitButton}`}>Cadastrar</button>
